acf7ee0113d4ab6a498c8bad34dfa19239d1bb95
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-11-16 17:57:16 -0300

    Update README and app.sh to enhance HTTP server instructions and logging

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gh-actions-docker
 Sample project demonstrating how to build a docker image and publish to docker hub using github actions.
+
+After building and publishing the docker image, run the container:
+
+```bash
+$ docker run -p 8080:8080 <DOCKER_USERNAME>/gh-actions-docker:latest
+```
+
+Open a web browser at http://localhost:8080/
+
+You should see a Hello World!
--- app.sh
+++ app.sh
@@ -5,7 +5,11 @@
 # The response includes the HTTP status line, the Content-Length header, and the response body
 # Upon response, the server waits for 1 second before closing the connection
 # The while loop ensures that the server keeps running with a new connection for each request
+
+echo "Starting HTTP server at port 8080"
+
 while true
 do
     echo -e "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello World!" | nc -l -p 8080 -q 1
+    echo "Request served"
 done
