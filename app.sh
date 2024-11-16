439dc863df0a405a4814de818217bf8028a963bb
Henrique Krausburg Correa <henrique.krausburg.correa@gmail.com>
2024-11-16 17:10:51 -0300

    Add a simple HTTP server script using netcat

---
+++ app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Start a simple HTTP server listening at port 8080 using netcat (nc)
+# The echo command sends an HTTP response to the client
+# The response includes the HTTP status line, the Content-Length header, and the response body
+# Upon response, the server waits for 1 second before closing the connection
+# The while loop ensures that the server keeps running with a new connection for each request
+while true
+do
+    echo -e "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello World!" | nc -l -p 8080 -q 1
+done
